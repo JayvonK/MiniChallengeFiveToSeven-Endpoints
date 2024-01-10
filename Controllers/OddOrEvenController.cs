aeccee59533d091538585bbff0a2f8e1fc1d3a99
JayvonK <jaykhuth@gmail.com>
2024-01-10 10:52:22 -0800

    method odd or even

--- Controllers/OddOrEvenController.cs
+++ Controllers/OddOrEvenController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MiniChallengeFiveToSeven_Endpoints.Services.OddOrEven;
 
 namespace MiniChallengeFiveToSeven_Endpoints.Controllers;
 
@@ -6,9 +7,16 @@ namespace MiniChallengeFiveToSeven_Endpoints.Controllers;
     [Route("[controller]")]
     public class OddOrEvenController : ControllerBase
     {
+    private readonly IOddOrEvenService _oddOrEvenService;
+
+    public OddOrEvenController(IOddOrEvenService oddOrEvenService)
+        {
+        _oddOrEvenService = oddOrEvenService;
+    }
+
         [HttpGet]
         [Route("EnterANumber/{number}")]
         public string OddOrEven(string number){
-            
+            return _oddOrEvenService.OddOrEven(number);
         }
     }
